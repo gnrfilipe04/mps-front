0dd11aa5606fd9c2a0b34abc9767ffb1b7504e4c
Filipe Souza <filipe.souza@doc360.com.br>
2021-10-05 11:21:33 -0300

    :sparkles: baseurl Alterado

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
+import https from "https";
 export const api = axios.create({
-    baseURL: `https://10.0.0.108:3333/`
+    baseURL: `https://10.0.0.108:3333/`,
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+    })
+      
 })
